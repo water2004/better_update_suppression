691c4c2435d416af17b917e720e62d9886c67b00
水源 <3076697508@qq.com>
2025-10-26 12:48:03 +0800

    添加物品名称、添加物品进入创造模式物品栏、更改异常类型为栈溢出

--- src/main/java/org/edtp/better_update_suppression/UpdateSuppressionBlock.java
+++ src/main/java/org/edtp/better_update_suppression/UpdateSuppressionBlock.java
@@ -16,12 +16,10 @@ import org.jetbrains.annotations.Nullable;
 
 public class UpdateSuppressionBlock extends Block {
     public static final BooleanProperty ACTIVED = BooleanProperty.of("actived");
-    protected boolean my_state;
 
     public UpdateSuppressionBlock(Settings settings) {
         super(settings);
         setDefaultState(getDefaultState().with(ACTIVED, false));
-        my_state=false;
     }
 
     @Override
@@ -33,10 +31,12 @@ public class UpdateSuppressionBlock extends Block {
     @Override
     protected ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
         if (!world.isClient()) {
-            my_state=!my_state;
+//            my_state=!my_state;
+            var active_state=state.get(ACTIVED);
+            active_state=!active_state;
             player.playSound(SoundEvents.BLOCK_RESPAWN_ANCHOR_CHARGE, 1, 1);
-            world.setBlockState(pos, state.with(ACTIVED, my_state));
-            if(my_state)
+            world.setBlockState(pos, state.with(ACTIVED, active_state));
+            if(active_state)
             {
                 player.sendMessage(Text.literal("update suppression on at: "+pos), false);
             }
@@ -54,19 +54,19 @@ public class UpdateSuppressionBlock extends Block {
         // 确保逻辑只在服务器端运行
         if (!world.isClient()) {
 
-            // 示例条件：当接收到来自钻石块的更新时，抑制后续所有更新
-            if (my_state) {
+            if (state.get(ACTIVED)) {
 
                 // 向附近玩家发送一条消息，告知即将发生抑制
                 var players = world.getPlayers();
                 for (PlayerEntity player : players) {
                     if (player.getBlockPos().isWithinDistance(pos, 16.0)) {
-                        player.sendMessage(Text.literal("Update suppressed by ExampleBlock at: "+pos), false);
+                        player.sendMessage(Text.literal("Update suppressed at: "+pos), false);
                     }
                 }
 
                 // 抛出我们的自定义异常来中断更新链
-                throw new UpdateSuppressionException("Intentionally suppressing block update chain at " + pos.toString());
+//                throw new UpdateSuppressionException("Intentionally suppressing block update chain at " + pos.toString());
+                throw new StackOverflowError("Intentionally suppressing block update chain at " + pos.toString());
             }
         }
 
--- src/main/java/org/edtp/better_update_suppression/UpdateSuppressionBlockFinal.java
+++ src/main/java/org/edtp/better_update_suppression/UpdateSuppressionBlockFinal.java
@@ -1,8 +1,10 @@
 package org.edtp.better_update_suppression;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.item.Items;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
@@ -11,7 +13,7 @@ import net.minecraft.util.Identifier;
 import java.util.function.Function;
 
 public class UpdateSuppressionBlockFinal {
-    public static final Block EXAMPLE_BLOCK = register("update_suppression_block", UpdateSuppressionBlock::new, Block.Settings.create().strength(0.1f));
+    public static final Block UPDATE_SUPPRESSION_BLOCK = register("update_suppression_block", UpdateSuppressionBlock::new, Block.Settings.create().strength(0.1f));
 
     private static Block register(String path, Function<AbstractBlock.Settings, Block> factory, AbstractBlock.Settings settings) {
         final Identifier identifier = Identifier.of("better_update_suppression", path);
@@ -23,5 +25,8 @@ public class UpdateSuppressionBlockFinal {
     }
 
     public static void init() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.REDSTONE).register(entries -> {
+            entries.add(UPDATE_SUPPRESSION_BLOCK);
+        });
     }
 }
